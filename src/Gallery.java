eaca2415f48f3b790c63ed9f49285f0426b95525
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-05 15:17:49 +0000

    Fixed an issue with the UI not updating properly
    - Added refreshUI() to the redraw gallery method
    - Moved JPanel drawing into if statement so we're only making the image if we actually have to

--- src/Gallery.java
+++ src/Gallery.java
@@ -25,32 +25,31 @@ public class Gallery extends JPanel {
     }
 
     public void addImage(BufferedImage image) {
-        Image resizedImage = image.getScaledInstance(250,250, Image.SCALE_SMOOTH);
-        image.getGraphics().drawImage(resizedImage,0,0, null);
-
-        JPanel imagePanel = new JPanel() {
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(resizedImage,0,0,null);
-            }
-        };
-
-        imagePanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                super.mouseClicked(e);
-                selectedImage = (JPanel) e.getComponent();
-                System.out.println(selectedImage);
-            }
-        });
 
         if (imagesList.size() > 11) {
             JOptionPane.showMessageDialog(this, "Gallery full, please delete an image", "Gallery full", JOptionPane.ERROR_MESSAGE);
         } else {
-            imagesList.add(imagePanel);
-        }
+            Image resizedImage = image.getScaledInstance(250,250, Image.SCALE_SMOOTH);
+            image.getGraphics().drawImage(resizedImage,0,0, null);
 
-        redrawGallery();
+            JPanel imagePanel = new JPanel() {
+                protected void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                    g.drawImage(resizedImage,0,0,null);
+                }
+            };
+
+            imagePanel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    super.mouseClicked(e);
+                    selectedImage = (JPanel) e.getComponent();
+                    System.out.println(selectedImage);
+                }
+            });
+            imagesList.add(imagePanel);
+            redrawGallery();
+        }
     }
 
     private void deleteSelectedImage() {
@@ -63,10 +62,9 @@ public class Gallery extends JPanel {
             if (selectedImage == panel) {
                 System.out.println("Found image");
                 it.remove();
+                redrawGallery();
             }
         }
-        redrawGallery();
-        repaint();
 
     }
 
@@ -75,6 +73,7 @@ public class Gallery extends JPanel {
         for (JPanel panel: imagesList) {
             imagesPanel.add(panel);
         }
+        imagesPanel.updateUI();
 
     }
 }
