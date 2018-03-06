0f34c975fbfcacbd54ede1a116f53c26fa79ba89
Kacper Martela <kacper99@hotmail.co.uk>
2018-03-06 00:40:25 +0000

    Fixed initial variables

--- src/DoilyDrawingArea.java
+++ src/DoilyDrawingArea.java
@@ -11,8 +11,8 @@ public class DoilyDrawingArea extends JPanel{
 
     private boolean showSectorLines = true;
     private boolean reflectDrawnPoints = true;
-    private int sectors = 16;
-    private int brushSize = 3;
+    private int sectors = 32;
+    private int brushSize = 5;
     private Color penColour = Color.WHITE;
     private Line line = new Line(this);
     private ArrayList<Line> lines = new ArrayList<>();
